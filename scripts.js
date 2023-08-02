f97238f65165e7b67c41c7e036375f56606f7563
Meet Patel <89350078+meet41@users.noreply.github.com>
2023-08-02 16:23:54 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
 class App {
     constructor() {
         this.tasks = JSON.parse(localStorage.getItem('Tasklist')) || [];
@@ -60,27 +61,23 @@ class App {
     }
 
     displayTasks() {
-        if (!this.$Tasklist) {
-            return;
-        }
-
-        const hasTask = this.tasks.length > 0;
-        this.$Tasklist.style.display = hasTask ? "flex" : "none";
-
-        this.$Tasklist.innerHTML = this.tasks
-            .map(
-                task => `
-                <div class="task-list" data-id="${task.id}">
-                    <div class="task-name">${task.name}</div>
-                    <div class="task-desc">${task.desc}</div>
-                    <div class="tools">
-                        <i style="font-size: 15px;margin:4px;align-items: center;justify-content: center;display:flex;" class="task-delete fas fa-trash-alt" data-id="${task.id}"></i>
-                    </div>
-                </div>
-            `
-            )
-            .join("");
-    }
+      const hasTask = this.tasks.length > 0;
+      this.$Tasklist.style.display = hasTask ? "flex" : "none";
+      this.$Tasklist.innerHTML = this.tasks
+        .map(
+          task => `
+            <div class="task-list" data-id="${task.id}">
+            <div class="task-name">${task.name}</div>
+            <div class="task-desc">${task.desc}</div>
+            <div class="tools">
+              <i style="font-size: 15px;margin:4px;align-items: center;justify-content: center;display:flex;" class="task-delete fas fa-trash-alt" data-id="${task.id}"></i>
+            </div>
+          </div>
+        `
+        )
+    .join("");
+}
 }
 
 new App();
+});
